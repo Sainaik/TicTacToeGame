30775bba39c5bdcdcad55633fbcdcc36aba82852
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-10 14:47:53 +0530

    [Sai]Added. Created TicTacToe Board

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -7,6 +7,21 @@ namespace TicTacToeGame
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!, Welcome to TicTac Toe Game");
+
+
+            char[] ticTacToeBoard = CreateTicTacToeBoard();
+        }
+
+        public static char[] CreateTicTacToeBoard()
+        {
+            char[] ticTacToeBoard = new char[10];
+
+            for(int i = 0; i< 10;i++)
+            {
+                ticTacToeBoard[i] = ' ';
+            }
+            
+            return ticTacToeBoard;
         }
     }
 }
