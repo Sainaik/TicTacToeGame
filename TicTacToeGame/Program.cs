880862617772ff70a6a094f988a7a179ff4c29e4
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-10 14:51:19 +0530

    [Sai]Refactor. Index Starts from 1

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -16,7 +16,7 @@ namespace TicTacToeGame
         {
             char[] ticTacToeBoard = new char[10];
 
-            for(int i = 0; i< 10;i++)
+            for(int i = 1; i< 10;i++)
             {
                 ticTacToeBoard[i] = ' ';
             }
